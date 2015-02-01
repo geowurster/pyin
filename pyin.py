87e13301dff1406857054fa1fc7f9b5b34fb3557
kevin Wurster <wursterk@gmail.com>
2015-02-01 13:51:40 -0500

    Unittests

--- pyin.py
+++ pyin.py
@@ -1,47 +1,114 @@
 """
-Perform Python operations on every line read from stdin
+Perform Python operations on every line streamed from stdin
 """
 
 
-import os
 import sys
 
 import click
 
 
-def pyin(stream, operation, strip=True):
+__version__ = '0.1.0'
+__author__ = 'Kevin Wurster'
+__email__ = 'wursterk@gmail.com'
+__source__ = 'https://github.com/geowurster/pyin'
+__license__ = '''
+New BSD License
+
+Copyright (c) 2014, Kevin D. Wurster
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* The names of its contributors may not be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+
+def pyin(stream, operation):
+
+    """
+    Read lines from an input stream and apply the same operation to every line.
+
+    Parameters
+    ----------
+    stream : file
+        File-like object that yields one line every iteration.
+    operation : string
+        Expression to be evaluated by `eval()`.  Lines are accessible via a
+        variable named 'line'.
+
+    Yields
+    ------
+    str
+        The result of `eval(operation)`.
+    """
 
     for line in stream:
-        if strip:
-            line = line.strip()
         yield eval(operation)
 
 
 @click.command()
 @click.option(
-    '-i', '--i-stream', metavar='STDIN', type=click.File(mode='r'), default=sys.stdin,
-    help="Input stream"
+    '-i', '--i-stream', metavar='STDIN', type=click.File(mode='r'),
+    help="Input stream."
 )
 @click.option(
-    '-o', '--o-stream', metavar='FILE', type=click.File(mode='w'), default=sys.stdout,
-    help="Output stream"
+    '-o', '--o-stream', metavar='FILE', type=click.File(mode='w'),
+    help="Output stream."
 )
 @click.option(
-    '-s / -ns', '--strip / --no-strip', is_flag=True, default=True,
-    help="Don't strip leading and trailing whitespace"
+    '-b', '--block', is_flag=True, default=False,
+    help="Apply operation to entire input."
 )
 @click.option(
-    '-n', '--newline', metavar='CHAR', default=os.linesep,
-    help="Newline character to append to every line"
+    '-im', '--import', 'import_modules', metavar='MODULE', multiple=True,
+    help="Import module"
 )
 @click.argument(
     'operation', type=click.STRING, required=True
 )
-def main(i_stream, operation, o_stream, strip, newline):
+@click.version_option(version=__version__)
+def main(i_stream, operation, o_stream, block, import_modules):
+
+    """
+    Perform Python operations on every line read from stdin.
+    """
 
     try:
-        for output in pyin(i_stream, operation, strip=strip):
-            o_stream.write(str(output) + newline)
+
+        for module in import_modules:
+            globals()[module] = __import__(module)
+
+        if block:
+            i_stream = [i_stream.read()]
+
+        # Have to kind of hack click.echo() in order to get it to work in the testing module and here
+        # For some reason using o_stream.write() works here but is not captured by the CliRunner()
+        # class that is used to test.  The argument 'file' sets the output stream to whatever the user specifies
+        # and since the input lines already have a newline character, setting 'nl' to an empty string prevents
+        # two newline characters from being written
+        for output in pyin(i_stream, operation):
+            click.echo(str(output), file=o_stream, nl='')
         sys.exit(0)
 
     except Exception as e:
---
+++ tests.py
@@ -0,0 +1,75 @@
+"""
+Unittests for pyin
+"""
+
+
+import os
+import tempfile
+import unittest
+
+from click.testing import CliRunner
+
+import pyin
+
+
+TEST_CONTENT = '''"field1","field2","field3"
+"l1f1","l1f2","l1f3"
+"l2f1","l2f2","l3f3"
+"l3f1","l3f2","l3f3"
+"l4f1","l4f2","l4f3"
+"l5f1","l5f2","l5f3"'''.strip()
+
+
+class TestPyin(unittest.TestCase):
+
+    def setUp(self):
+        self.tempfile = tempfile.NamedTemporaryFile(mode='r+')
+        self.tempfile.write(TEST_CONTENT)
+        self.tempfile.seek(0)
+
+    def tearDown(self):
+        self.tempfile.close()
+
+    def test_pass_through(self):
+        actual = "".join(line for line in pyin.pyin(self.tempfile, 'line'))
+        self.assertEqual(TEST_CONTENT, actual)
+
+    def test_replace_quotes(self):
+        actual = "".join(line for line in pyin.pyin(self.tempfile, """line.replace('"', "'")"""))
+        expected = TEST_CONTENT.replace('"', "'")
+        self.assertEqual(expected, actual)
+
+
+class TestCli(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+        self.tempfile = tempfile.NamedTemporaryFile(mode='r+')
+        self.tempfile.write(TEST_CONTENT)
+        self.tempfile.seek(0)
+
+    def tearDown(self):
+        self.tempfile.close()
+    
+    def test_pass_through(self):
+        result = self.runner.invoke(pyin.main, ['-i', self.tempfile.name, "line"])
+        self.assertEqual(0, result.exit_code)
+        self.assertEqual(TEST_CONTENT, result.output)
+
+    def test_exception(self):
+        malformed_operation = "__lin__--"
+        result = self.runner.invoke(pyin.main, ['-i', self.tempfile.name, malformed_operation])
+        self.assertNotEqual(0, result.exit_code)
+
+    def test_change_newline(self):
+        nl = '__NL__'
+        result = self.runner.invoke(pyin.main,
+                                    ['-i', self.tempfile.name, "line.replace(os.linesep, '%s')" % nl, '-im', 'os'])
+        self.assertEqual(0, result.exit_code)
+        expected = TEST_CONTENT.replace(os.linesep, nl)
+        self.assertEqual(expected.strip(), result.output.strip())
+
+    def test_block(self):
+        result = self.runner.invoke(pyin.main, ['-i', self.tempfile.name, "'nothing'", '--block'])
+        self.assertEqual(0, result.exit_code)
+        self.assertEqual('nothing', result.output.strip())
