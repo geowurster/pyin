3bfd312a63033bafaaec72999360db3250d16705
kevin Wurster <wursterk@gmail.com>
2015-02-01 13:51:50 -0500

    Updated

--- setup.py
+++ setup.py
@@ -2,19 +2,45 @@
 
 
 """
-Setup script for str2type
+Setup script for pyin
 """
 
 
 import setuptools
 
+import pyin
+
+
+with open('README.md') as f:
+    readme = f.read().strip()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip()
+
 
 setuptools.setup(
-    name='pyin',
-    py_modules=['pyin'],
-    zip_safe=True,
+    author=pyin.__author__,
+    author_email=pyin.__email__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: General',
+        'Topic :: Utilities'
+    ],
+    description="streaming text processing",
     entry_points="""
         [console_scripts]
         pyin=pyin:main
     """,
+    include_package_data=True,
+    install_requires=install_requires,
+    license=pyin.__license__,
+    long_description=readme,
+    name=pyin.__name__,
+    py_modules=['pyin'],
+    url=pyin.__source__,
+    version=pyin.__version__,
+    zip_safe=True,
 )
