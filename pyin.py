1ab21080708fa8ae2d23a2c95716f11a72b09f99
Kevin Wurster <kevin@skytruth.org>
2015-02-11 11:03:22 -0500

    Version bump - accidentally uploaded a bad version to PyPi.

--- pyin.py
+++ pyin.py
@@ -15,7 +15,7 @@ import str2type as _str2type
 __all__ = ['pyin']
 
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __author__ = 'Kevin Wurster'
 __email__ = 'wursterk@gmail.com'
 __source__ = 'https://github.com/geowurster/pyin'
