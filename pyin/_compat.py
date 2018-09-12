28e92fc26871d9d4d2250b7dbfce39ecbcb86fcc
Kevin Wurster <wursterk@gmail.com>
2018-09-11 22:53:02 -0400

    More Python 2/3 compatibility.

--- pyin/_compat.py
+++ pyin/_compat.py
@@ -3,12 +3,17 @@ Python 2 vs. 3 compatibility
 """
 
 
+import itertools as it
 import sys
 
 
 if sys.version_info.major == 2:  # pragma no cover
     text_type = unicode
     string_types = basestring,
+    filter = it.ifilter
+    map = it.imap
 else:  # pragma no cover
     text_type = str
     string_types = str,
+    filter = filter
+    map = map
