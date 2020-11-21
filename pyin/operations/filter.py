28958393b5bf5c8fc93e1d5d3c67184706a63bfd
Kevin Wurster <wursterk@gmail.com>
2020-11-21 00:33:44 -0500

    %filterfalse %ff

--- pyin/operations/filter.py
+++ pyin/operations/filter.py
@@ -8,8 +8,26 @@ class Filter(Eval):
 
     """Filter items against an expression."""
 
-    directives = ('%filter', '%filt')
+    directives = (
+        '%filter', '%filt',
+        '%filterfalse', '%ff')
     kwargs = OrderedDict([('expr', str)])
+
+    @classmethod
+    def cli_help(cls, directive):
+
+        if directive in ('%filter', '%filt'):
+            return """
+            Only keep items in the stream where the expression evaluates
+            as 'True'.
+            """
+        elif directive in ('%filterfalse', '%ff'):
+            return """
+            Drop items from the stream where the expression evaluates as
+            'False'.
+            """
+        else:
+            raise RuntimeError("Unexpected directive: {}".format(directive))
 
     def __init__(self, directive, variable, global_scope, expr):
 
@@ -25,4 +43,8 @@ class Filter(Eval):
     def __call__(self, stream):
         stream, selection = it.tee(stream, 2)
         selection = super(Filter, self).__call__(selection)
+
+        if self.directive in ('%filterfalse', '%ff'):
+            selection = (not s for s in selection)
+
         return it.compress(stream, selection)
