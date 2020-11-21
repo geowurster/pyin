2ae7d0bae8aad5654f57d42b7268320bd78de991
Kevin Wurster <wursterk@gmail.com>
2020-11-21 00:10:05 -0500

    More builting casting options

--- pyin/operations/builtins.py
+++ pyin/operations/builtins.py
@@ -5,12 +5,22 @@ class Cast(BaseOperation):
 
     """Cast to a builtin type."""
 
-    directives = ('%list', '%l')
+    directives = (
+        '%list', '%l',
+        '%str', '%s',
+        '%int', '%i', '%d',
+        '%float', '%f')
 
     def __call__(self, stream):
 
         mapping = {
-            ('%list', '%l'): list
+            ('%float',  '%f'): float,
+            ('%int',    '%i'): int,
+            ('%list',   '%l'): list,
+            ('%str',    '%s'): str,
+            ('%tuple',  '%t'): tuple,
+            ('%dict',   '%d'): dict,
+            ('%set',        ): set
         }
 
         func = mapping[self.directive]
