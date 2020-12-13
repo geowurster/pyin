00213ee3b3d60f499a61d957e2c65c6a9d267f7c
Kevin Wurster <wursterk@gmail.com>
2020-12-12 21:42:44 -0500

    %reverse

--- pyin/operations/iter.py
+++ pyin/operations/iter.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 import itertools as it
+from types import GeneratorType
 
+from pyin import _compat
 from pyin.operations.base import BaseOperation
 
 
@@ -46,3 +48,33 @@ class Slice(BaseOperation):
                 yield list(islice(stream, chunksize))
             except StopIteration:
                 break
+
+
+class Reverse(BaseOperation):
+
+    """Reverse each item with ``%rev`` and the entire stream with ``%revs``."""
+
+    directives = ('%rev', '%revs')
+
+    def __init__(self, *args, **kwargs):
+        super(Reverse, self).__init__(*args, **kwargs)
+        self.reverse_by_index = tuple(list(_compat.string_types) + [list, tuple])
+
+    def reverse(self, item):
+        if isinstance(item, self.reverse_by_index):
+            return item[::-1]
+        elif isinstance(item, GeneratorType):
+            return (i for i in reversed(tuple(item)))
+        else:
+            return reversed(item)
+
+    def __call__(self, stream):
+
+        if self.directive == '%revs':
+            for item in self.reverse(stream):
+                yield item
+        elif self.directive == '%rev':
+            for item in _compat.map(self.reverse, stream):
+                yield item
+        else:
+            self.raise_invalid_directive()
