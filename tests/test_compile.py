c21af7a5eb12df0350cd2367c7839c4e5dd35a36
Kevin Wurster <wursterk@gmail.com>
2023-10-21 11:47:01 -0400

    Improve OpBase() validation of subclasses
    We can do a _lot_ of validation at the time the subclass is defined.

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -24,25 +24,3 @@ def test_missing_argument():
         list(pyin.eval('%eval', []))
 
     assert "missing argument 'expression' for directive: %eval" in str(e.value)
-
-
-def test_missing_annotation():
-
-    """Directives with missing arg type annotations should raise an exception.
-    """
-
-    class OpBroken(pyin.OpBase, directives=('%broken', )):
-
-        def __init__(self, directive: str, arg, /, **kwargs):
-            super().__init__(directive, **kwargs)
-            self.arg = arg
-
-        def __call__(self, stream):
-            return stream
-
-    with pytest.raises(RuntimeError) as e:
-        list(pyin.eval(['%broken', 'value'], []))
-
-    assert 'missing a type annotation' in str(e.value)
-    assert "argument 'arg'" in str(e.value)
-    assert "directive '%broken'" in str(e.value)
