ddf3837b1b384f99a61c9a85f4210eca494e7ba2
Kevin Wurster <wursterk@gmail.com>
2023-10-21 11:35:04 -0400

    Better documentation

--- pyin.py
+++ pyin.py
@@ -1,4 +1,4 @@
-"""It's like ``sed``, but Python!"""
+"""Like ``sed``, but Python."""
 
 
 import abc
@@ -195,16 +195,17 @@ def importer(expressions, scope):
 
     """Parse expressions and import modules into a single scope.
 
-    All :obj:`ImportError`s are suppressed. It is pretty much impossible to
-    know if a given module is expected to be importable, but in the event
-    a module really does fail to import, a :obj:`NameError` will be raised at
-    runtime when an expression references an object that does not exist.
+    An expression might be something like ``"os.path.exists(i)"``. This
+    function parses that expression and imports ``os.path`` into ``scope``.
+    Expressions are evaluated by Python's eval within this scope.
 
-    :param expressions:
-        Look through these expressions and attempt to import anything that
-        looks like a reference to a module.
-    :param scope:
-        Import modules into this scope. For use with Python's :func:`eval`.
+    :param str or sequence expressions:
+        One or more Python expression.
+    :param dict scope:
+        Track imported objects in this scope. Typically, all imports are
+        written to a single scope.
+
+    :rtype dict:
     """
 
     # Find all potential modules to try and import
@@ -247,132 +248,24 @@ def eval(
         stream_variable=_DEFAULT_STREAM_VARIABLE
 ):
 
-    """Map Python expressions across a stream of data.
+    """Evaluate Python expressions across a stream of data.
 
-    .. warning::
+    Expressions are passed through ``importer()`` to construct a scope, and
+    then evaluated one-by-one across each item in ``stream`` by Python's
+    ``eval()``.
 
-        This function makes heavy use of Python's :func:`eval`, so a Python
-        expression evaluated by this function should be one that you are
-        comfortable passing to Python's :func:`eval`. Mostly this means that
-        only trusted code should be executed, but be sure to read the
-        appropriate Python docs.
+    :param str or sequence expressions:
+        One or more expressions.
+    :param iterable stream:
+        Map all ``expressions`` across each item.
+    :param str variable:
+        Each item in ``stream`` should be stored in this variable in the
+        scope.
+    :param str stream_variable:
+        Like ``variable`` but for referencing ``stream`` itself.
 
-        One major difference is that the input expressions are examined, and
-        any referenced modules are imported dynamically. For example, an
-        expression like ``"os.path.exists(line)"`` would cause ``os.path.exists``
-        to be imported.
-
-    Expressions can access the current line being processed with a variable
-    called `line`. The goal is to emulate being dropped inside a ``for`` loop
-    with limited overhead, and maximum access to Python libraries.
-
-    For example, this:
-
-    .. code-block:: python
-
-        for line in iterable:
-            if 'mouse' in line:
-                line = line.upper()
-            if 'cat' in line:
-                yield line
-
-    would be expressed as:
-
-    .. code-block:: python
-
-        expressions = (
-            "line.upper() if 'mouse' in var else line",
-            "'cat' in var")
-        pyin.eval(expressions, iterable, var='line')
-
-    Expressions can be used for a limited amount of control flow and filtering
-    depending on what comes out of :func:`eval`. There are 3 types of objects
-    that an expression can return, and in some cases the object stored in
-    ``variable`` is changed:
-
-    * ``True`` - The original object from the ``iterator`` is passed to the
-      next expression for evaluation. If the last expression produces `True`
-      the object is yielded. Whatever is currently stored in ``variable``
-      remains unchanged.
-    * ``False`` or ``None`` - Expression evaluation halts and processing moves
-      on to the next object from `iterator`.
-    * ``object`` - Expression results that aren't `True` or `False` are placed
-      inside the ``variable`` name for subsequent expressions.
-
-    So, use expressions that evaluate as ``True`` or ``False`` to filter lines
-    and anything else to transform content.
-
-    Consider the following example CSV and expressions using ``line`` as
-    ``variable``:
-
-    .. code-blocK::
-
-        "field1","field2","field3"
-        "l1f1","l1f2","l1f3"
-        "l2f1","l2f2","l3f3"
-        "l3f1","l3f2","l3f3"
-        "l4f1","l4f2","l4f3"
-        "l5f1","l5f2","l5f3"
-
-    This expression:
-
-    .. code-block::
-
-        "'field' in line"
-
-    is equivalent to:
-
-    .. code-block:: python
-
-        for line in iterator:
-            if 'field' in line:
-                yield line
-
-    and would only return the first line because it evaluates as ``True`` and
-    the rest evaluate as ``False``.
-
-    To capitalize the second line the expression:
-
-    .. code-block::
-
-        "line.upper() if 'l2' in line"
-
-    would be equivalent to:
-
-    .. code-block:: python
-
-        for line in iterator:
-           if 'l2' in line:
-               line = line.upper()
-
-    Convert to JSON encoded lists using this odd expression:
-
-    .. code-block::
-
-        "map(lambda x: x.replace('\"', ''), line.strip().split(','))"
-
-    Equivalent to this Python code:
-
-    .. code-block:: python
-
-        for line in iterator:
-            line = line.strip().replace('"', '').strip()
-            yield line.split(',')
-
-    Parameters
-    ----------
-    expressions : str or tuple
-        A single Python expression or tuple containing multiple expressions.
-        Expressions are Python code that can reference external modules, which
-        will automatically be imported.
-    iterable : hasattr('__iter__')
-        An iterator producing one object to be evaluated per iteration.
-    variable : str, optional
-        Expressions reference this variable to access objects from `iterator`
-        during processing.
-    stream_variable : str, optional
-        Some expressions are evaluated against the stream itself. Place the
-        stream in this variable.
+    :return:
+        An iterator of results.
     """
 
     scope = {
@@ -429,6 +322,29 @@ class OpBase(abc.ABC):
     Subclassers can use positional-only arguments and type annotations in
     ``__init__`` to define arguments associated with the directive and
     their type.
+
+    Subclassers are free to reference a variety of attributes on their instance
+    that contain a variety of information about how they should execute:
+
+
+    directive
+      The directive currently being executed.
+
+    directives
+      A list of all supported directives.
+
+    variable
+      When executing a Python expression, place the item currently being
+      processed into this environment in the scope for Python's builtin
+      ``eval()``. When only evaluating an expression against an item (and not
+      the full ``stream`` object), it is good to not use ``stream_variable``.
+
+    stream_variable
+      Like ``variable`` but for the entire ``stream`` object.
+
+    scope
+      Use this as the global scope when exeucting expressions with Python's
+      builtin ``eval()`` function.
     """
 
     directives = None
@@ -541,6 +457,10 @@ class OpBase(abc.ABC):
         2. Consume all items in ``stream``.
         3. Be a generator or return an iterator.
 
+        An implementation should also be conscious of function call overhead.
+        ``pyin`` primarily seeks to be friendly and convenient, but fast is
+        also nice.
+
         :param stream:
             Input data stream. An iterable object.
 
@@ -553,22 +473,39 @@ class OpBase(abc.ABC):
 
 class OpEval(OpBase, directives=('%eval', )):
 
-    """Evaluate a Python expression with :obj:`eval`.
+    """Evaluate a Python expression with Python's ``eval()``.
 
-    .. note::
+    This operation receives special treatment in ``compile()``, but its
+    subclassers do not. When parsing the input expressions, anything not
+    associated with a directive is assumed to be a generic Python expression
+    that should be handled by this class.
 
-        This operation receives special treatment in :func:`compile`, but its
-        subclassers do not. When parsing the input expressions, any expression
-        not associated with a directive is assumed to be a generic Python
-        expression that should be handled by this class. Users may still use
-        the ``%eval`` directive.
+    In code terms, this:
+
+    .. code:: python
+
+        >>> import pyin
+        >>> list(pyin.eval('i + 1', range(3)))
+        [1, 2, 3]
+
+    is equivalent to:
+
+    .. code:: python
+
+        >>> import pyin
+        >>> list(pyin.eval(['%eval', 'i + 1'], range(3)))
+        [1, 2, 3]
     """
 
     def __init__(self, directive: str, expression: str, /, **kwargs):
 
         """
+        :param str directive:
+            See parent implementation.
         :param expression:
-            Python expression.
+            Evaluate this expression with Python's ``eval()``.
+        :param **kwargs kwargs:
+            For parent ``__init__()``.
         """
 
         super().__init__(directive, **kwargs)
@@ -633,14 +570,16 @@ class OpAccumulate(OpBase, directives=('%accumulate', )):
     """Accumulate the entire stream into a single object."""
 
     def __call__(self, stream):
+
         yield list(stream)
 
 
 class OpChain(OpBase, directives=('%chain', )):
 
-    """Flatten the stream by one level – like :obj:`itertools.chain`."""
+    """Flatten the stream by one level – like ``itertools.chain()``."""
 
     def __call__(self, stream):
+
         return it.chain.from_iterable(stream)
 
 
@@ -821,17 +760,6 @@ class OpStrNoArgs(OpBase, directives=(
 
     def __call__(self, stream):
 
-        """Process data.
-
-        :param iterable stream:
-            The data stream.
-
-        :rtype str:
-
-        :return:
-            A new string.
-        """
-
         return map(op.methodcaller(self.directive[1:]), stream)
 
 
@@ -852,8 +780,7 @@ class OpStrOneArg(OpBase, directives=(
 
     def __init__(self, directive: str, argument: str, /, **kwargs):
 
-        """``OpStrOneArg()`` constructor.
-
+        """
         :param str directive:
             Working with this directive.
         :param str argument:
@@ -867,15 +794,6 @@ class OpStrOneArg(OpBase, directives=(
         self.argument = argument
 
     def __call__(self, stream):
-
-        """Process data.
-
-        :param iterable stream:
-            The data stream.
-
-        :return:
-            An iterable of new strings.
-        """
 
         mapping = {
             '%strips': 'strip',
@@ -902,8 +820,7 @@ class OpReplace(OpBase, directives=('%replace', )):
 
     def __init__(self, directive: str, old: str, new: str, /, **kwargs):
 
-        """``OpReplace()`` constructor.
-
+        """
         :param str directive:
             Currently active directive.
         :param str old:
@@ -921,62 +838,11 @@ class OpReplace(OpBase, directives=('%replace', )):
 
     def __call__(self, stream):
 
-        """Process data.
-
-        :param iterable stream:
-            The data stream.
-
-        :return:
-            Replaced strings.
-        """
-
         return map(op.methodcaller('replace', self.old, self.new), stream)
 
 
 ###############################################################################
 # Command Line Interface
-
-
-_CLI_DESCRIPTION = r"""
-It's like sed, but Python!
-
-Map Python expressions across lines of text.  If an expression evaluates as
-'False' or 'None' then the current line is thrown away.  If an expression
-evaluates as 'True' then the next expression is evaluated.  If a list or
-dictionary is encountered it is JSON encoded.  All other objects are cast
-to string.
-
-Newline characters are stripped from the end of each line before processing
-and are added on write unless disabled with '--no-newline'.
-
-This utility employs 'eval()' internally but uses a limited scope to help
-prevent accidental side effects, but there are plenty of ways to get around
-this so don't pass anything through pyin that you wouldn't pass through
-'eval()'.
-
-Remove lines that do not contain a specific word:
-
-    $ pyin -i file "'word' in line"
-
-Capitalize lines containing a specific word:
-
-    $ pyin -i file "line.upper() if 'word' in line else line"
-
-Only print every other word from lines that contain a specific word:
-
-    $ pyin \
-      -i file \
-      "'word' in line" \
-      "line.split()[::2])" \
-      "' '.join(line)"
-
-The '$ pyin' expressions in the command above do:
-
-    1. Select only lines containing 'word'.
-    2. Split string on whitespace and select every other word. Note that this
-       expression results in a list.
-    3. Join list of words into a string.
-""".strip()
 
 
 def _type_variable(value):
@@ -1013,7 +879,7 @@ def argparse_parser():
     """
 
     aparser = argparse.ArgumentParser(
-        description=_CLI_DESCRIPTION,
+        description="Like sed, but Python!",
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
@@ -1081,17 +947,17 @@ def argparse_parser():
 
 def _adjust_sys_path(f):
 
-    """Decorator to ensure :attr:`sys.path` is adjusted properly.
+    """Adjust ``sys.path`` to include nearby files.
 
     Being able to reference Python files or modules in the current directory
-    is very powerful, but requires an adjustment :attr:`sys.path` that we to
+    is very powerful, but requires an adjustment ``sys.path`` that we to
     only manifest in certain contexts.
 
-    Primarily, applying this to :func:`main` allows it to provide an interface
-    to the CLI from within Python that also includes the :attr:`sys.path`
+    Primarily, applying this to ``main()`` allows it to provide an interface
+    to the CLI from within Python that also includes the ``sys.path``
     adjustment.
 
-    :param f:
+    :param callable f:
         Function to wrap.
     """
 
@@ -1122,24 +988,25 @@ def main(
 
     """Command line interface.
 
-    Direct access to the CLI logic. :obj:`argparse.ArgumentParser` can be
-    accessed with :func:`parser`.
+    Direct access to the CLI logic. See ``argparse_parser()`` for a compatible
+    parser. Returns an exit code, but does not suppress all exceptions.
 
-    :param generate_expr:
+    :param str generate_expr:
         Generate input data from this expression instead of ``infile``.
-    :param infile:
+    :param file infile:
         Read text from this file.
-    :param outfile:
+    :param file outfile:
         Write text to this file.
-    :param expressions:
-        Evaluate these ``pyin`` expressions on each line of text from
-        ``infile``.
-    :param linesep:
-        Write this after every line.
-    :param variable:
-        Place each input item in this variable when evaluating expressions.
-    :param stream_variable:
-        See ``--variable``.
+    :param sequence expressions:
+        Evaluate these expressions.
+    :param str linesep:
+        Postfix for each output line.
+    :param str variable:
+        Expressions reference input data via this variable.
+    :param str stream_variable:
+        Expressions reference the stream via this variable.
+
+    :rtype int:
 
     :returns:
         Exit code.
@@ -1192,15 +1059,20 @@ def main(
 
 
 def _cli_entrypoint(rawargs=None):
-    """Shim for CLI entrypoint.
 
-    :func:`main` and :func:`parser` provide an entrypoint to the CLI that can
-    also be invoked from Python, but does not work well with ``setuptools``'s
-    entrypoint machinery. This shim provides a bridge.
+    """Command-line interface entrypoint.
+
+    ``main()`` and ``argparse_parser()`` provide the tooling needed to use
+    the ``$ pyin`` utility's logic from within Python. This layer handles
+    some error conditions that are closer to the shell than Python.
+
+    Raises ``SystemExit`` instead of returning a value.
 
     :param rawargs:
         Like :obj:`sys.argv` (used by default) but without the interpreter
         path. Used in testing.
+
+    :raises SystemExit:
     """
 
     args = argparse_parser().parse_args(args=rawargs)
